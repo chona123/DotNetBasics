396184833014d68d8f80aa27a3403200c14292d1
Ramya Chockalingam <ramya.chockalingam@blackboard.com>
2021-10-26 23:18:12 +0530

    MVC assessment1

---
+++ MvcEcart/MvcEcart/viewdataModel/itemviewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Web.Mvc;
+
+namespace MvcEcart.viewdataModel
+{
+    public class itemviewModel
+    {
+        public Guid itemId { get; set; }
+        public int categoryId { get; set; }
+        public string itemCode { get; set; }
+        public string itemName { get; set; }
+        public string description { get; set; }
+        public decimal itemPrice { get; set; }
+        public HttpPostedFileBase imagePath { get; set; }
+       
+        public IEnumerable<SelectListItem> categorySelectListItem { get; set; }
+    }
+}
